8ca6f408221f6f5b834200e0e490fe3a4cdc4760
Meow <108517390+MeowMeow0417@users.noreply.github.com>
2025-05-03 19:50:50 +0800

    *Addedd: Electron Build to nextJs*

---
+++ electron/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000'); // Dev server
+  } else {
+    win.loadURL('http://localhost:3000'); // Serve it yourself or use a local server like express
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
